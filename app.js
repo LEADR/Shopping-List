89733469220fae27724606b162dd9bb139bc017d
LEADR <ian.fennie@gmail.com>
2014-09-23 23:00:44 -0700

    EOD [09/23]
    JS needs work

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   // Adding List Items
-  var addListItem = function() {
+  addListItem = function() {
     var text = $('#input-field').val();
 
     var checkbox = $('<input>').attr('type', 'checkbox');
@@ -17,16 +17,25 @@ $(document).ready(function() {
   $('#add-item').on('click', addListItem);
   $('#input-field').keydown(function(event) {
     if (event.which == '13') {
-      var text = $('#input-field').val();
-
-      var checkbox = $('<input>').attr('type', 'checkbox');
-      var item = $('<span>').addClass('item-name').html(text);
-      var deleteButton = $('<input>').attr('type', 'button').addClass('button').addClass('delete').val('X');
-
-      var listItem = $("<li>").append(checkbox).append(item).append(deleteButton);
-      $('#items-list').append(listItem);
-
-      $('#input-field').val('');
+      addListItem();
     }
   });
+
+  // Change text-decoration via checkbox
+  $('.checkbox').change(function(event) {
+    var itemName = $(this).closest('li').children('.item-name');
+
+
+    if ( null /* change from null */
+      // check for "checked" on $(this)
+    ) {
+      $(itemName).addClass('checked');
+    } else {
+      $(itemName).removeClass('checked');
+    }
+  });
+
+  // Add delete button functionality
+  // Add confirmation alert
+    // Add "don't ask again" checkbox
 });
